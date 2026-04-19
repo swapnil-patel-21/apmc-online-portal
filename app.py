a142e2ea7c34c3f6054066d304e49079eafc245a
Swapnil Patel <swapniljp211099@gmail.com>
2026-04-19 15:03:24 +0530

    Fix table creation

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from base import app, db
+from models import *
+
+# ✅ This will run when app starts (even on Render)
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host="0.0.0.0", port=5000)
